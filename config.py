ef7501ea0200d5df5a6df5ecada5f7ab0a6a5f6f
Zeren Kavaz <zerenkavaz@gmail.com>
2025-12-23 03:08:04 +0300

    son version

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 DEFAULT_LOCATION = "Moda, Kadikoy"
 BG_COLOR = "#202020"
 ROAD_COLOR = "#404040"
-WINDOW_TITLE = "Operasyon ve Analiz"
+WINDOW_TITLE = "Afet Rota Optimizasyon Sistemi"
 WINDOW_SIZE = (1200, 800)
